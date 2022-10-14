386e2869a35b0b6e695b19a2900e3b04744ac898
Michael Dorst <m@mdorst.net>
2022-10-13 23:19:05 -0700

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    hyprland = {
+      url = "github:hyprwm/Hyprland";
+      # build with your own instance of nixpkgs
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = { self, nixpkgs, hyprland }: {
+    nixosConfigurations.scout = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        hyprland.nixosModules.default
+        { programs.hyprland.enable = true; }
+      ];
+    };
+  };
+}
